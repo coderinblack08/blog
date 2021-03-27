aedb7d426b7a6abc1bfb0bc9e2e160e24af459d1
coderinblack <kevinlu.email@gmail.com>
2021-03-26 23:10:04 -0700

    Remove breaking feature

--- pages/api/view/[slug].ts
+++ pages/api/view/[slug].ts
@@ -1,18 +1,11 @@
-import fs from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
 import { redis } from "../../../lib/redis";
 
 const view = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug } = req.query;
-
-  const slugs = fs.readdirSync("posts");
-  if (slugs.filter((file) => file.replace(".md", "") === slug).length > 0) {
-    const key = `views:${slug}`;
-
-    await redis.incr(key);
-    return res.status(200).send("success");
-  }
-  return res.status(400).send({ error: "Article doesn't exist" });
+  const key = `views:${slug}`;
+  await redis.incr(key);
+  return res.status(200).send("success");
 };
 
 export default view;
