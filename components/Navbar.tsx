3bfc080a6f65873c22c4cc112c1bd4d967d833e8
coderinblack <kevinlu.email@gmail.com>
2021-06-27 17:05:21 -0700

    Add github and contact links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,14 +11,22 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     <nav className="flex justify-between items-center py-8">
       <Image src={cloud} alt="Cloud Icon" width={64} height={64} />
       <div className="flex items-center space-x-2">
-        <button className="flex items-center bg-white border border-gray-200 py-1.5 px-5 rounded-lg text-orange-400">
+        <a
+          href="https://github.com/coderinblack08"
+          target="_blank"
+          className="flex items-center bg-white border border-gray-200 py-1.5 px-5 rounded-lg text-orange-400"
+          rel="noreferrer"
+        >
           <Image src={profile} alt="Profile Icon" width={16} height={16} />
           <span className="ml-2 font-medium">GitHub</span>
-        </button>
-        <button className="flex items-center bg-white border border-gray-200 py-1.5 px-5 rounded-lg text-orange-400">
+        </a>
+        <a
+          href="mailto:kevin@affordance.app"
+          className="flex items-center bg-white border border-gray-200 py-1.5 px-5 rounded-lg text-orange-400"
+        >
           <Image src={message} alt="Message Icon" width={16} height={16} />
           <span className="ml-2 font-medium">Contact</span>
-        </button>
+        </a>
       </div>
     </nav>
   );
